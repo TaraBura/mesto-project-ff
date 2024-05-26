f441c48372cf79a9d3a048312542d088011bdb7c
Egor Tarabukin <egor-tarabukin@yandex.ru>
2024-05-26 17:46:57 +0300

    Add and delete cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const content = document.querySelector ('.content');
+const addButton = content.querySelector('.profile__add-button');
+const containerCards = content.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function addCard(cardData, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__image').alt = cardData.name;
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', () => deleteCard(cardElement));
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(cardItem) {
+  cardItem.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(card => containerCards.append(addCard(card, deleteCard)));
+
